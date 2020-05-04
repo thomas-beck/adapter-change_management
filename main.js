8e6169766d30df1c68b24c193c98d911155f67fc
thomas-beck <thomas.beck@centurylink.com>
2020-05-04 19:17:57 +0000

    Final Project

--- main.js
+++ main.js
@@ -206,6 +206,8 @@ healthcheck(callback) {
     var workEnd = jsonData.result[0].work_end;
     var sys_id = jsonData.result[0].sys_id;
    newJSON = { "change_ticket_number" : number, "active" : active , "priority" : priority , "description" : description , "work_start" : workStart , "work_end" : workEnd , "change_ticket_key" : sys_id };
+   newJSON = Object.keys(newJSON).map(function(key) { return [ Number(key), newJSON[key]];
+   });
    _processedData = newJSON;
     }
     }
@@ -243,7 +245,7 @@ healthcheck(callback) {
     var workEnd = jsonData.result[0].work_end;
     var sys_id = jsonData.result[0].sys_id;
    newJSON = { "change_ticket_number" : number, "active" : active , "priority" : priority , "description" : description , "work_start" : workStart , "work_end" : workEnd , "change_ticket_key" : sys_id };
-   _processedData = JSON.parse(newJSON);
+   _processedData = newJSON;
     }
     callback(_processedData, _processedError, _response);
     });
