a335114bb7688d4c7533832e427a67994d5abc8a
thomas-beck <thomas.beck@centurylink.com>
2020-05-04 18:53:07 +0000

    Final Project

--- main.js
+++ main.js
@@ -194,7 +194,7 @@ healthcheck(callback) {
   this.connector.get((_processedData, _processedError) => { 
     var detectObject = typeof _processedData;
     var _response = _processedData;
-    if(_processedData.body.includes(body)) {
+    if(_processedData.body.includes('result')) {
     var recordCount =  1;
     for( var i = 0; i < recordCount; i++) {   
     let jsonData = JSON.parse(_processedData.body);
